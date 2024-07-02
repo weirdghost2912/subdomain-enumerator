faa58fd76a5bf40495c853b062425abb488098b3
weirdghost2912 <76821126+weirdghost2912@users.noreply.github.com>
2024-07-02 18:20:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
-domain=input()
-
+domain=input('Enter domain:  ')
+wd=input('Enter wordlist:  ')
+with open(wd,'r') as f:
+    file=f.read()
 
 def each_req(i,domain):
     r=requests.get(i+'.'+domain)
-    if r.get==200:
+    if r.status_code==200:
         dict
 def enum_req():
     for i in file:
