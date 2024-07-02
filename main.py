19d113f93950f70c69de5011ee129b9a8c748b08
weirdghost2912 <76821126+weirdghost2912@users.noreply.github.com>
2024-07-02 18:15:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+domain=input()
+
+
+def each_req(i,domain):
+    r=requests.get(i+'.'+domain)
+    if r.get==200:
+        dict
+def enum_req():
+    for i in file:
+        each_req(i,domain)
+
+
+
+
+
+
+
